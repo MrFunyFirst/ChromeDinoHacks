c3e6738087b839ac7ca75ab56f0a2f1c019813f7
MrFunyFirst <98651893+MrFunyFirst@users.noreply.github.com>
2023-02-03 11:02:11 +0300

    Add files via upload

---
+++ chromeDinoHack.js
@@ -0,0 +1,49 @@
+let trueGameOver = Object.assign(Runner.instance_.gameOver, {});
+
+document.addEventListener("keydown", (ev) => {
+
+    switch (ev.key) {
+
+        case "ArrowLeft":
+
+            Runner.instance_.currentSpeed -= 2;
+
+            break;
+
+        case "ArrowRight":
+
+            Runner.instance_.currentSpeed += 2;
+
+            break;
+
+        case "p":
+
+            alert("Game paused, click enter to resume");
+
+            break;
+
+        case "g":
+
+            if (Runner.instance_.gameOver != trueGameOver) {
+
+                Runner.instance_.gameOver = trueGameOver;
+
+                document.title = 'dino (god mod disabled)';
+
+            } else {
+
+                Runner.instance_.gameOver = () => {};
+
+                document.title = 'dino (god mod enabled)';
+
+            }
+
+            break;
+
+        default:
+
+            break;
+
+    }
+
+})
